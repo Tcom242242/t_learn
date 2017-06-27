71d5f5ac561362b18c16d3787ef4fa3ec189d673
tcom <okano565656@gmail.com>
2017-06-27 18:46:34 +0900

    add description of example of k_means

--- examples/k_means.rb
+++ examples/k_means.rb
@@ -1,5 +1,7 @@
 require "t_learn"
+require "pry"
 
 k_means = TLearn::K_Means.new()
 data_list = JSON.load(open("./sample_1dim.json"))
-history = k_means.fit(data_list)
+history = k_means.fit(data_list, c=3) # => {result:=> Array, history => Array}
+# history[:result][0] => {id=>cluster's number, :vec=>cluster's point, :v_list=> clustered data list}
